bac0e6571285bf9da0c449dbe9484f4781199e16
H-Deen <hdeenthegreat@gmail.com>
2025-04-16 23:02:10 +0500

    fix: removed unused prop

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -5,7 +5,7 @@ interface TagProps {
   className?: string;
 }
 
-export const Tag : React.FC<TagProps> = ({children, className = ""}) => {
+export const Tag : React.FC<TagProps> = ({children}) => {
   return (
     <div className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">{children}</div>
   )
